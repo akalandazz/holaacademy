ed3530246a0b75af6b72c6d7a5ee9ac8a63a1004
amiran <amurikokalandia@gmail.com>
2025-01-06 01:30:19 +0400

    added contact page

--- home/forms.py
+++ home/forms.py
@@ -5,3 +5,19 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = Contact
         fields = ['name', 'email', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'w-full px-3 py-2 text-gray-900 rounded',
+                'placeholder': 'Your Name',
+            }),
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full px-3 py-2 text-gray-900 rounded',
+                'placeholder': 'Your Email',
+            }),
+            'message': forms.Textarea(attrs={
+                'class': 'w-full px-3 py-2 text-gray-900 rounded',
+                'placeholder': 'Your Message',
+                'rows': 5,
+                'cols': 40,
+            }),
+        }
