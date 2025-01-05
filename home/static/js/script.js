10639ed27a459231057598623165c9e485b55a87
amiran <amurikokalandia@gmail.com>
2025-01-06 00:46:00 +0400

    created landing page

--- home/static/js/script.js
+++ home/static/js/script.js
@@ -1,4 +1,12 @@
 // static/js/script.js
 document.addEventListener('DOMContentLoaded', () => {
     console.log('JavaScript is working!');
+    document.getElementById('mobile-menu-button').addEventListener('click', function() {
+        var menu = document.getElementById('mobile-menu');
+        if (menu.classList.contains('hidden')) {
+            menu.classList.remove('hidden');
+        } else {
+            menu.classList.add('hidden');
+        }
+    });
 });
